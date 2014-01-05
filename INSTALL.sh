a42e62fa6b1789e37ff0b0997459a72b4ce4b54a
Henry Escobar <henry.escobar@gmail.com>
2014-01-05 13:04:19 -0500

    Simple Install script; initial tmux conf for screen users

---
+++ INSTALL.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# Geek: Henry J. Escobar
+# Purpose: Copy the contents of my shell enviroment from github into home dir
+# Notes;
+#	Tried to use find to build file list but had problems with sub dirs.
+#	Will need to try again later
+#
+#       Debating with self if being explict is better or not
+#
+# Bugs: 
+
+homeFileList=".bashrc .tmux.conf"
+sshSubDir="config"
+
+backupTag=`date +%Y%m%d-%H%M%S`
+
+
+for i in $homeFileist ; do
+    if [ -f ~/$i ] ; then
+       echo "cp ~/$i ~/$i-$backupTag"
+    fi
+    echo "cp $i ~/"
+done
+
+for i in $sshSubDir ; do
+    if [ ! -d ~/.ssh ] ; then
+       mkdir ~/.ssh
+       chmod 0700 ~/.ssh
+    fi 
+
+    if [ -f ~/.ssh/$i ] ; then
+       echo "cp ~/.ssh/$i ~/.ssh/$i-$backupTag"
+    fi
+
+    echo "cp $i ~/.ssh/"
+done
